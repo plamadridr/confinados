57e29c823d53c808fa29dafbbba389546b0346a3
Patricia Lamadrid <cf18patricia.lamadrid@iesjoandaustria.org>
2020-05-17 17:11:06 +0200

    Render App component.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//const element = document.createElement('h1')
+//element.innerText = "Hello React"
+//const container = document.getElementById('root')
+//container.appendChild(element)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './components/App'
+
+//const element = <h1>Hello React II</h1>
+const container = document.getElementById('root')
+
+// ReactDom.render(_que_, _on_)
+//ReactDom.render(element, container)
+ReactDom.render(<App/>, container)
