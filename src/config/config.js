6cc2a61089668474dc2992b5fc5cd9ee9fa2b9bc
Patricia Lamadrid <cf18patricia.lamadrid@iesjoandaustria.org>
2020-05-17 17:09:23 +0200

    Config database firebase.

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyC1p1TXG3i4YQmsOd8A0SKMsyM4SVuEDQQ",
+    authDomain: "confinados-8c94f.firebaseapp.com",
+    databaseURL: "https://confinados-8c94f.firebaseio.com",
+    projectId: "confinados-8c94f",
+    storageBucket: "confinados-8c94f.appspot.com",
+    messagingSenderId: "956200899690",
+    appId: "1:956200899690:web:2edb95b9ec282b1fb3e613",
+    measurementId: "G-D18Y6L4361"
+
+}
